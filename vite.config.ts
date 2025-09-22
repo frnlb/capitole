7008f0ed637db11574fd7bcf0352fc37b5051c6a
fran <franlobogarcia@gmail.com>
2025-09-22 13:14:55 +0200

    feat: add basic structure & add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@helpers": path.resolve(__dirname, "src/helpers"),
+      "@types": path.resolve(__dirname, "src/types"),
+    },
+  },
 });
