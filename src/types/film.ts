b6222dd9d13c79c3ff09d74a316b9a8ce0e6c3c4
fran <franlobogarcia@gmail.com>
2025-09-22 18:11:57 +0200

    feat: add fetch popular movies & films type

---
+++ src/types/film.ts
@@ -0,0 +1,38 @@
+export interface Film {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  popularity: number;
+  adult: boolean;
+  video: boolean;
+}
+
+export interface FilmDetails extends Film {
+  genres: Genre[];
+  runtime: number;
+  budget: number;
+  revenue: number;
+  production_companies: ProductionCompany[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export type FilmCategory = "popular" | "top_rated" | "upcoming";
