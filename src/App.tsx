ce48d795dbaf87bb68fb9bfa4aba62d2f85b2b9c
fran <franlobogarcia@gmail.com>
2025-09-23 13:30:30 +0200

    feat: add styles - draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.scss";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { Home } from "@/pages";
+import { Layout } from "@/components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <Layout>
       <Home />
-    </>
+    </Layout>
   );
 }
 
