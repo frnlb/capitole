4f38f4fb732126d5b604c9d9b1e78670ec80747d
fran <franlobogarcia@gmail.com>
2025-09-23 11:48:29 +0200

    feat: add store

---
+++ src/store/wishlist.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { Film } from "@/types";
+
+interface WishlistState {
+  items: Film[];
+  addToWishlist: (film: Film) => void;
+  removeFromWishlist: (filmId: number) => void;
+  isInWishlist: (filmId: number) => boolean;
+  clearWishlist: () => void;
+}
+
+export const useWishlistStore = create<WishlistState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      addToWishlist: (film) =>
+        set((state) => ({
+          items: state.items.some((item) => item.id === film.id)
+            ? state.items
+            : [...state.items, film],
+        })),
+      removeFromWishlist: (filmId) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== filmId),
+        })),
+      isInWishlist: (filmId) => get().items.some((item) => item.id === filmId),
+      clearWishlist: () => set({ items: [] }),
+    }),
+    {
+      name: "film-wishlist",
+    }
+  )
+);
