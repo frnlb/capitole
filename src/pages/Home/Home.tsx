4235b82f4ea00bfeb4ce68660e4909fa23794c87
fran <franlobogarcia@gmail.com>
2025-09-24 11:50:51 +0200

    feat: clean home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,43 +1,9 @@
-import React, { useState } from "react";
-import { useFilms } from "@/hooks/useFilms";
 import "./Home.scss";
 
 export const Home: React.FC = () => {
-  const popularFilms = useFilms("popular");
-  console.log("🚀 ~ Home ~ popularFilms:", popularFilms);
-  // const topRatedFilms = useFilms("top_rated");
-  // const upcomingFilms = useFilms("upcoming");
-
-  // const popFilms = popularFilms.results?.map((film) => {
-  //   console.log("film: ", film);
-  //   return (
-  //     <div>
-  //       <h1>This should be a film</h1>
-  //       <img src={film.poster_path}></img>
-  //       <h3>{film.title}</h3>
-  //     </div>
-  //   );
-  // });
-
-  const { films, loading, error } = useFilms("popular");
-  console.log("🚀 ~ Home ~ error:", error);
-  console.log("🚀 ~ Home ~ loading:", loading);
-  console.log("🚀 ~ Home ~ films:", films);
-  const popFilms = films.results.map((film) => {
-    console.log("film: ", film);
-    return (
-      <div>
-        <h1>This should be a film</h1>
-        <img src={film.poster_path ?? ""}></img>
-        <h3>{film.title}</h3>
-      </div>
-    );
-  });
-
   return (
     <div className="page">
       <h1>Home alone</h1>
-      {popFilms}
     </div>
   );
 };
