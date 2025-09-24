ba76e8a1b06b92527c3b827bbfa4daea305f0fc3
fran <franlobogarcia@gmail.com>
2025-09-24 10:07:39 +0200

    feat: add icon & button

---
+++ src/types/styles.ts
@@ -0,0 +1,11 @@
+export type Colors =
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "text"
+  | "text-secondary"
+  | "background"
+  | "surface"
+  | "popular"
+  | "top-rated"
+  | "upcoming";
