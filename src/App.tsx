ae1b60fba11c53d78f35c7ccedc973f2f9b35411
fran <franlobogarcia@gmail.com>
2025-09-24 17:01:24 +0200

    fix: remove log

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Layout } from "@/components";
 import { SsrData } from "./entry-server";
 
 function App({ initialData }: { initialData?: SsrData }) {
-  console.log("\ninitialData \ninside App: \n--------->", initialData);
   return (
     <Layout>
       <Home films={initialData?.films} />
