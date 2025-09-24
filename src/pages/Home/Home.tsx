2152280ed24503017e8c5e1133206007af913b96
fran <franlobogarcia@gmail.com>
2025-09-24 12:53:20 +0200

    feat: add home props

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { Film } from "@/types";
 import "./Home.scss";
 
-export const Home: React.FC = () => {
+export const Home: React.FC<{ films: { results: Film[] } }> = ({ films }) => {
+  const popFilms = films.results?.map((film) => (
+    <div key={film.id}>
+      <h1>This should be a film</h1>
+      <img src={film.poster_path ?? ""} alt={film.title}></img>
+      <h3>{film.title}</h3>
+    </div>
+  ));
+
   return (
     <div className="page">
-      <h1>Home alone</h1>
+      <h1>Home</h1>
+      {popFilms}
     </div>
   );
 };
